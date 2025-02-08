b9d930e8e95d6617bd7059a15086006c806f2dc6
Gustavo Weber <weber1.caxias@gmail.com>
2025-02-08 13:25:20 -0300

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://tidy-beagle-98.clerk.accounts.dev",
+            domain: "https://clerk.jogodabandeira.com.br",
             applicationID: "convex",
         }
     ]
